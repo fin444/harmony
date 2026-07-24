589c9b2320195bf04f5429e8cfa3a2b152302af7
fin444 <fin444@users.noreply.github.com>
2026-07-24 17:40:38 -0400

    database schema

---
+++ database-init.sql
@@ -0,0 +1,43 @@
+drop table if exists "message" cascade;
+drop table if exists "group_user" cascade;
+drop table if exists "channel" cascade;
+drop table if exists "group" cascade;
+drop table if exists "user" cascade;
+drop table if exists "file" cascade;
+
+create table "file" (
+	"id" integer primary key generated always as identity,
+	"name" varchar(40) not null
+);
+
+create table "group" (
+	"id" integer primary key generated always as identity,
+	"name" varchar(40) not null
+);
+
+create table "user" (
+	"id" integer primary key generated always as identity,
+	"username" varchar(20) not null,
+	"password" varchar(32) not null,
+	"pfpId" integer not null references "file"("id")
+);
+
+create table "group_user" (
+	"userId" integer not null references "user"("id"),
+	"groupId" integer not null references "group"("id")
+);
+
+create table "channel" (
+	"id" integer primary key generated always as identity,
+	"groupId" integer not null references "group"("id"),
+	"name" varchar(40) not null
+);
+
+create table "message" (
+	"id" integer primary key generated always as identity,
+	"userId" integer not null references "user"("id"),
+	"channelId" integer not null references "channel"("id"),
+	"fileId" integer references "file"("id"),
+	"content" varchar(512) not null,
+	"timestamp" timestamp not null
+);
