74a0b56ee6fc55f46959549bb6a706371a1d5038
James Weggel <jrw442@drexel.edu>
2026-08-01 16:04:44 -0400

    frontend/index: login/signup functionality complete

--- app/public/login.js
+++ app/public/login.js
@@ -1,10 +1,25 @@
 let usernameField = document.getElementById("username");
 let passwordField = document.getElementById("password");
 
-let submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", onSubmitButtonPress);
+let loginButton = document.getElementById("login");
+loginButton.addEventListener("click", onLogin);
+
+let signupButton = document.getElementById("signup");
+signupButton.addEventListener("click", onSignup);
 
 let stateText = document.getElementById("state");
+
+function errorStateText(text) {
+    stateText.style.color = "red";
+    stateText.textContent = text;
+}
+
+function setStateText(text) {
+    stateText.style.color = "black";
+    stateText.textContent = text;
+}
+
+setStateText("");
 
 function loginFetch(username, password){
     fetch(`/login?username=${username}&password=${password}`, {
@@ -13,11 +28,36 @@ function loginFetch(username, password){
         },
     }).then(response => {
         console.log("RESPONSE HEADER: ", response);
+        if(!response.ok) {
+            throw(response);
+        }
         return response.text();
     }).then(body => {
         appFetch(body);
     }).catch(error => {
         console.log("ERROR OCCURRED: ", error);
+        errorStateText(error.statusText);
+    });
+}
+
+
+
+function signupFetch(username, password){
+    fetch(`/signup?username=${username}&password=${password}`, {
+        method: 'GET', 
+        headers: {
+        },
+    }).then(response => {
+        console.log("RESPONSE HEADER: ", response);
+        if(!response.ok) {
+            throw(response);
+        }
+        return response.text();
+    }).then(body => {
+        appFetch(body);
+    }).catch(error => {
+        console.log("ERROR OCCURRED: ", error);
+        errorStateText(error.statusText);
     });
 }
 
@@ -27,21 +67,33 @@ function appFetch(token) {
 }
 
 
-function onSubmitButtonPress(event) {
+function onLogin(event) {
     event.preventDefault();
-    stateText.style.color = "black";
-    stateText.textContent = "";
+    setStateText("");
     if(usernameField.value.length === 0) {
-        stateText.style.color = "red";
-        stateText.textContent += "Please enter a username\n";
+        errorStateText("Please enter a username");
         return;
     }
     if(passwordField.value.length === 0) {
-        stateText.style.color = "red";
-        stateText.textContent += "Please enter a password\n";
+        errorStateText("Please enter a password");
         return;
     }
-    stateText.textContent = "Loading...";
+    setStateText("Loading...");
     loginFetch(usernameField.value, passwordField.value);
+}
+
+function onSignup(event) {
+    event.preventDefault();
+    setStateText("");
+    if(usernameField.value.length === 0) {
+        errorStateText("Please enter a username");
+        return;
+    }
+    if(passwordField.value.length === 0) {
+        errorStateText("Please enter a password");
+        return;
+    }
+    setStateText("Loading...");
+    signupFetch(usernameField.value, passwordField.value);
 }
 
