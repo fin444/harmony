31365e6f58289173f56c46016da81669941a57fb
James Weggel <jrw442@drexel.edu>
2026-08-05 12:43:52 -0400

    frontend/app: more socket functionality foundation, organized code

--- app/public/app.js
+++ app/public/app.js
@@ -1,87 +1,139 @@
-const tempImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOaa8Hmv8r-hqG31BpFaSI-AlPdkFTnIeLHNbKgJVTYCRsm3zMR28O8nMT&s=10";
+// Socket stuff
+
 const socket = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port)
-const handlers = {
-    invalidToken: function () {
-
-    },
-    groupList: function () {
-
-    },
-    groupInfo: function () {
-
-    },
-    messages: function () {
-
-    },
-    typingIndicator: function () {
-
-    },
-    userInfo: function () {
-
-    }
+const receiveHandlers = {
+    invalidToken:       function () {},
+    groupList:          function (groups) {},
+    groupInfo:          function (id, channels) {},
+    messages:           function (channelId, messages) {},
+    typingIndicator:    function (channelId, usersTyping) {},
+    userInfo:           function (id, name, pfpId) {}
 
 };
-
-
-const messageArea = document.getElementById("messages");
-const messageInputField = document.getElementById("type");
-const messageSendButton = document.getElementById("send");
+const sendHandlers = {
+    token:          function (tok) {},
+    getGroupInfo:   function (id) {},
+    getMessages:    function (channelId, index) {},
+    typingStatus:   function (isTyping) {},
+    sendMessage:    function (channelId, contents, fileId) {},
+    getUserInfo:    function (username) {},
+    setPfp:         function (fileId) {},
+    createThing:    function (thingType, name) {},
+    renameThing:    function (thingType, id, name) {},
+    deleteThing:    function (thingType, id) {},
+    inviteUser:     function (groupId, userId) {},
+};
 
 socket.addEventListener("message", (event) => {
 	console.log("message from server:", event.data)
-    switch (event.data.type) {
-
+    let data = JSON.parse(event.data);
+    switch (data.type) {
+        case "invalidToken":
+            receiveHandlers.invalidToken();
+            break;
+        case "groupList":
+            receiveHandlers.groupList(data.groups);
+            break;
+        case "groupInfo":
+            receiveHandlers.groupInfo(data.id, data.channels);
+            break;
+        case "messages":
+            receiveHandlers.messages(data.channelId, data.messages);
+            break;
+        case "typingIndicator":
+            receiveHandlers.typingIndicator(data.channelId, data.usersTyping);
+            break;
+        case "userInfo":
+            receiveHandlers.userInfo(data.id, data.name, data.pfpId);
+            break;
+        default:
+            return;
     }
 });
 
+// Constants
+
+const messageInputFieldElm = document.getElementById("type");
+const messageSendButtonElm = document.getElementById("send");
+const sidebarProfileImageElm = document.getElementById("profile-image");
+const messageAreaElm = document.getElementById("messages");
+
+// Variables
+
+let ownerPfp = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOaa8Hmv8r-hqG31BpFaSI-AlPdkFTnIeLHNbKgJVTYCRsm3zMR28O8nMT&s=10";
+let ownerUsername = "test_curuser";
+let otherPfp = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOaa8Hmv8r-hqG31BpFaSI-AlPdkFTnIeLHNbKgJVTYCRsm3zMR28O8nMT&s=10";
+let otherUsername = "test_other"
+
+// Functions
+
+function setMessageView(chatName, groupImg, messages) {
+    // document.getElementById("header-chat-name").textContent = chatName;
+    // // TODO Set header image
+    // messageInputField.placeholder = "Message " + chatName;
+
+    // let messageAreaElm = document.getElementById("messages");
+    // messageAreaElm.replaceChildren();
+    // for(message in messages) {
+
+    // }
+    
+}
 
 
-function createMessageDiv(message, username, pfpHref, timestamp, file) {
+function getMessageDiv(message, username, pfpUrl, timestamp, file) {
     let parentElm = document.createElement("div");
     parentElm.className = "chat-element-div";
 
-    let timestampField = document.createElement("p");
-    timestampField.className = "chat-timestamp";
-    timestampField.textContent = timestamp;
+    let timestampElm = document.createElement("p");
+    timestampElm.className = "chat-timestamp";
+    timestampElm.textContent = timestamp;
 
-    let messageField = document.createElement("p");
-    messageField.className = "chat-message";
-    messageField.textContent = message;
+    let bodyElm = document.createElement("p");
+    bodyElm.className = "chat-message";
+    bodyElm.textContent = message;
 
-    let usernameField = document.createElement("p");
-    usernameField.className = "chat-username";
-    usernameField.textContent = username;
+    let usernameElm = document.createElement("p");
+    usernameElm.className = "chat-username";
+    usernameElm.textContent = username;
 
-    let profilePic = document.createElement("img");
-    profilePic.className = "chat-profile-pic";
-    profilePic.src = pfpHref;
+    let pfpElm = document.createElement("img");
+    pfpElm.className = "chat-profile-pic";
+    pfpElm.src = pfpUrl;
 
-    parentElm.append(timestampField);
-    parentElm.append(profilePic);
-    parentElm.append(usernameField);
-    parentElm.append(messageField);
+    // TODO: FILE ATTACHMENTS
+
+    parentElm.append(timestampElm);
+    parentElm.append(pfpElm);
+    parentElm.append(usernameElm);
+    parentElm.append(bodyElm);
 
     return parentElm;
 }
 
+function sendMessage() {
+    let messageText = messageInputFieldElm.value;
+    messageInputFieldElm.value = "";
+    // TEMPORARY FUNCTIONALITY TEST - THIS WILL BE CHANGED!!
+    if (messageText !== "") messageAreaElm.prepend(getMessageDiv(messageText, ownerUsername, ownerPfp, Date.now(), ""));
+}
 
-let profileImageElm = document.getElementById("profile-image");
-profileImageElm.src = tempImg;
+function initializePage () {
+    sidebarProfileImageElm.src = ownerPfp;
 
-messageSendButton.addEventListener("click", sendMessage);
-messageInputField.addEventListener("keydown", function(event) {
+    // Test alr exiting messages
+    messageAreaElm.prepend(getMessageDiv("hi", ownerUsername, ownerPfp, "4 Aug 2026 13:59:59", ""));
+    messageAreaElm.prepend(getMessageDiv("hi 2", otherUsername, otherPfp, "4 Aug 2026 14:00:02", ""));
+}
+
+// Event listeners
+
+messageSendButtonElm.addEventListener("click", sendMessage);
+messageInputFieldElm.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
     sendMessage();
   }
 });
 
-function sendMessage() {
-    let messageText = messageInputField.value;
-    messageInputField.value = "";
-    // TEMPORARY FUNCTIONALITY TEST - THIS WILL BE CHANGED!!
-    if (messageText !== "") messageArea.prepend(createMessageDiv(messageText, "james", tempImg, Date.now(), ""));
-}
+initializePage();
 
-
-messageArea.prepend(createMessageDiv("hi", "james", tempImg, "4 Aug 2026 13:59:59", ""));
-messageArea.prepend(createMessageDiv("hi 2", "james 2", tempImg, "4 Aug 2026 14:00:02", ""));
