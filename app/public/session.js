de3eb128013074b937b6fd60a031a25a35df912f
fin444 <fin444@users.noreply.github.com>
2026-08-22 18:23:05 -0400

    frontend/session: pass message index to dom

--- app/public/session.js
+++ app/public/session.js
@@ -55,7 +55,7 @@ export function addMessageToQueue(message){
             }
             let userMatch = userCache[this.message.userId];
             let username = userMatch ? userMatch.name : "Unknown";
-            let messageDiv = getElement.messageDiv(this.message.contents, username, this.message.userId, this.message.timestamp, this.message.fileId);
+            let messageDiv = getElement.messageDiv(this.message.contents, username, this.message.userId, this.message.timestamp, this.message.fileId, this.message.index);
             prependMessage(messageDiv);
             
         }
